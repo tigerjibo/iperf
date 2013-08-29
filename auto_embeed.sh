e1f0e46f51a44d0ff198bab6ad9286eb6a887c0d
Ji Bo <jibo@edan.com>
2013-08-29 19:40:10 +0800

    add auto_embeed.sh to auto generate config.h file next: (1)modify config.h,comment #define malloc rpl_malloc (2)make (3)make install iperf in /usr/local/bin folder Signed-off-by: Ji Bo <jibo@edan.com>

---
+++ auto_embeed.sh
@@ -0,0 +1,3 @@
+./configure --host=arm-linux \
+CC=/opt/freescale/usr/local/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc \
+CXX=/opt/freescale/usr/local/gcc-4.4.4-glibc-2.11.1-multilib-1.0/arm-fsl-linux-gnueabi/bin/arm-none-linux-gnueabi-g++
